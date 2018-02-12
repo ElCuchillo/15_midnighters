2f0124f37ffcf356ba0671f96eb21cfc3a568207
ElCuchillo <32874706+ElCuchillo@users.noreply.github.com>
2018-02-12 20:18:51 +0300

    username doubles removed

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,50 +1,60 @@
 import requests
 import pytz
 from datetime import datetime, date, time, timedelta
+from itertools import groupby
 
 
-def load_attempts(pages=10):
+def load_attempts():
     url = 'https://devman.org/api/challenges/solution_attempts'
-    for page in range(1, pages):
+    page, number_of_pages = 1, 1
+    while page <= number_of_pages:
         pay_load = {'page': page}
-        response = requests.get(url, params=pay_load).json()['records']
-        for user in response:
+        json_data = requests.get(url, params=pay_load).json()
+        for attempt in json_data['records']:
             yield {
-                'username': user['username'],
-                'timestamp': user['timestamp'],
-                'timezone': user['timezone'],
+                'username': attempt['username'],
+                'timestamp': attempt['timestamp'],
+                'timezone': attempt['timezone'],
             }
+        number_of_pages = int(json_data['number_of_pages'])
+        page += 1
 
 
-def midnight_test(attempt, night_hours):
-    timezone = pytz.timezone(attempt['timezone'])
-    attempt_time = pytz.utc.localize(
-        datetime.utcfromtimestamp(attempt['timestamp'])).astimezone(timezone)
-    midnight = timezone.localize(
-        datetime.combine(attempt_time.date(), time(0, 0)))
-    morning = midnight + timedelta(hours=night_hours)
-    return attempt_time > midnight and attempt_time <= morning
+def get_local_time(timestamp, tz):
+    timezone = pytz.timezone(tz)
+    return datetime.fromtimestamp(timestamp, tz=timezone)
+
+
+def is_midnight(attempt, night_hours):
+    attempt_time = get_local_time(attempt['timestamp'], attempt['timezone'])
+    return attempt_time.hour < night_hours
 
 
 def get_midnighters(attempts_list, night_hours):
-    midnighters_list = [attempt for attempt in attempts_list
-        if midnight_test(attempt, night_hours)]
-    return midnighters_list
+    night_owls = [(
+        attempt['username'],
+        attempt['timestamp'],
+        attempt['timezone']
+        )
+        for attempt in attempts_list if is_midnight(attempt, night_hours)
+    ]
+    return night_owls
 
 
-def print_midnighters(midnighters_list, night_hours):
+def print_midnighters(night_owls, night_hours):
     print("DEVMAN's Night Owls, who made their attmepts "
-          "since 24:00 till {}:00 are:\n".format(night_hours))
-    for user in midnighters_list:
-        timezone = pytz.timezone(user['timezone'])
-        user_time = pytz.utc.localize(
-            datetime.utcfromtimestamp(user['timestamp'])).astimezone(timezone)
-        print('{} at {}, timezone {}'.format(user['username'],
-            user_time.strftime('%Y-%m-%d %H:%M %Z%z'), user['timezone']))
+          "since 24:00 till {}:00 are:".format(night_hours))
+    night_owls.sort(key=lambda x: x[0])
+    for user, attempt in groupby(night_owls, lambda x: x[0]):
+        print('\n{}:'.format(user))
+        for name, time, tz in attempt:
+            user_time = get_local_time(time, tz)
+            print('   {}'.
+                format(user_time.strftime('%Y/%m/%d %H:%M %Z%z')))
 
 
 if __name__ == '__main__':
     night_hours = 6
-    attempts_list = load_attempts()
-    midnighters_list = get_midnighters(attempts_list, night_hours)
-    print_midnighters(midnighters_list, night_hours)
+    attempts_list = list(load_attempts())
+    night_owls = get_midnighters(attempts_list, night_hours)
+    print_midnighters(night_owls, night_hours)
